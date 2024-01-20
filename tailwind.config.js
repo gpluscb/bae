2aaaa6dcfd11eba3de05ee02c3ee8ea21fda8442
gpluscb <20143778+gpluscb@users.noreply.github.com>
2024-01-20 12:11:55 +0100

    Do *some* frontend work

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./web_contents/templates/**/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['"Montserrat"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      'heading': ['"Quicksand"', '"Montserrat"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+    },
   },
   plugins: [],
 }

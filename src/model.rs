ddab631c9ef35a16d810e38264e94463b2e13341
gpluscb <20143778+gpluscb@users.noreply.github.com>
2024-01-27 13:25:53 +0100

    start work on infrastructure for uploading blog posts

--- src/model.rs
+++ src/model.rs
@@ -3,6 +3,27 @@ use chrono::{DateTime, Duration, Utc};
 use serde::Deserialize;
 use std::fmt::{Display, Formatter};
 
+/// Probably slightly low-ball estimate but that's fine, it's a technical blog.
+const AVERAGE_READING_WPM: usize = 200;
+
+#[derive(Clone, Eq, PartialEq, Debug, Deserialize)]
+pub enum MdOrHtml {
+    Markdown(String),
+    Html(String),
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Deserialize)]
+pub struct PartialBlogPost {
+    pub url: String,
+    pub title: String,
+    pub description: String,
+    pub author: Author,
+    pub contents: MdOrHtml,
+    pub tags: Vec<Tag>,
+    pub accessible: bool,
+    pub publication_date: Option<DateTime<Utc>>,
+}
+
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct BlogPost {
     pub url: String,
@@ -22,6 +43,76 @@ pub struct Author(pub String);
 
 #[derive(Clone, Eq, PartialEq, Debug, Deserialize)]
 pub struct Tag(pub String);
+
+fn generate_reading_time(content: &str) -> Duration {
+    Duration::minutes((content.split_whitespace().count() / AVERAGE_READING_WPM) as i64)
+}
+
+fn generate_html_from_md(markdown: &str) -> String {
+    todo!()
+}
+
+impl MdOrHtml {
+    pub fn contents(&self) -> &str {
+        match self {
+            MdOrHtml::Markdown(contents) => contents,
+            MdOrHtml::Html(contents) => contents,
+        }
+    }
+
+    pub fn markdown(&self) -> Option<&str> {
+        if let MdOrHtml::Markdown(md) = self {
+            Some(md)
+        } else {
+            None
+        }
+    }
+
+    pub fn html(&self) -> Option<&str> {
+        if let MdOrHtml::Html(html) = self {
+            Some(html)
+        } else {
+            None
+        }
+    }
+}
+
+impl PartialBlogPost {
+    pub fn generate_blog_post(self) -> BlogPost {
+        let PartialBlogPost {
+            url,
+            title,
+            description,
+            author,
+            contents,
+            tags,
+            accessible,
+            publication_date,
+        } = self;
+
+        let reading_time = generate_reading_time(contents.contents());
+        let (markdown, html) = match contents {
+            MdOrHtml::Markdown(md) => {
+                let html = generate_html_from_md(&md);
+                (Some(md), html)
+            }
+            MdOrHtml::Html(html) => (None, html),
+        };
+
+        BlogPost {
+            url,
+            title,
+            description,
+            author,
+            markdown,
+            html,
+            tags,
+            reading_time,
+            accessible,
+            publication_date,
+        }
+    }
+}
 
 impl BlogPost {
     pub fn full_path(&self) -> BlogPostPath {
